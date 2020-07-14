5f90963083e4432e390c031fdc75137334e4a695
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-07-14 15:41:50 +0530

    Deploy Next.js to master

---
+++ _next/static/Ga4oeNxFLZXHm7Mr2NhPy/pages/about.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{QKkg:function(n,o,u){"use strict";u.r(o);o.default=function(){return"About"}},xXHS:function(n,o,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u("QKkg")}])}},[["xXHS",0]]]);
